784d6282bfd64c30e0ff583fc565eb7a4712d351
Brandon Milton <brandon.milton21@gmail.com>
2014-01-10 22:25:30 -0800

    Added menuicon. Added list for lap. Completed lap functionality. Completed lap list. Realization of static keyword. Only need to add back button functionality for lap list.

--- src/SimpleStopwatch.c
+++ src/SimpleStopwatch.c
@@ -3,12 +3,15 @@
 static Window *window;
 static TextLayer *text_layer;
 static ActionBarLayer *action_bar;
+static SimpleMenuLayer *lap_layer;
 static GBitmap *my_icon_pause, *my_icon_reset, *my_icon_lap, *my_icon_start;
 static int seconds = 0;
 static int minutes = 0;
 static int hours = 0;
-static bool paused = true;
-
+static int currentLapIndex = 0;
+static bool paused = true; static bool inLapMenu = false; static char lapTimes[100][12];
+static SimpleMenuSection menu_sections[1];
+static SimpleMenuItem menu_items[100];
 
 static char * vtom(int sec, int min, int hour)
 {
@@ -26,6 +29,37 @@ static char * vtom(int sec, int min, int hour)
   }
   return arr;
 }
+
+static char * itom(int time)
+{
+  static char arr[] = "00h:00m:00s";
+  int i = 9;
+  while (time > 0)
+  {
+    arr[i] = (time % 10) + '0';
+    time /= 10;
+    if (i % 4 == 1)
+      i--;
+    else if (i % 4 == 0)
+      i-=3;
+  } 
+  return &arr[0];
+}
+
+void vitom(char* arr, int time)
+{
+  int i = 9;
+  while (time > 0)
+  {
+    arr[i] = (time % 10) + '0';
+    time /= 10;
+    if (i % 4 == 1)
+      i--;
+    else if (i % 4 == 0)
+      i-=3;
+  }
+}
+
 
 static void switch_resume_icon()
 {
@@ -59,7 +93,23 @@ static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed)
 
 void lap_handle(ClickRecognizerRef recognizer, void *context)
 {
-  text_layer_set_text(text_layer, "LAP!");
+  int i = currentLapIndex;  
+  int time = (hours*10000) + (minutes*100) + seconds;
+  lapTimes[i][0] = '0';
+  lapTimes[i][1] = '0';
+  lapTimes[i][2] = 'h';
+  lapTimes[i][3] = ':';
+  lapTimes[i][4] = '0';
+  lapTimes[i][5] = '0';
+  lapTimes[i][6] = 'm';
+  lapTimes[i][7] = ':';
+  lapTimes[i][8] = '0';
+  lapTimes[i][9] = '0';
+  lapTimes[i][10] = 's';
+  lapTimes[i][11] = '\0';
+  
+  vitom(lapTimes[i], time);
+  currentLapIndex++;
 }
 
 void pause_handle(ClickRecognizerRef recognizer, void *context)
@@ -76,11 +126,70 @@ void reset_handle(ClickRecognizerRef recognizer, void *context)
   text_layer_set_text(text_layer, "00h\n00m\n00s");
 }
 
+void back_handle(ClickRecognizerRef recognizer, void *context)
+{
+  text_layer_set_text(text_layer, "LOL!");
+  if (inLapMenu)
+  {
+    //Deinit lap menu
+    inLapMenu = false;
+    Layer* lap_layer_temp = simple_menu_layer_get_layer(lap_layer);
+    layer_remove_from_parent(lap_layer_temp);
+    simple_menu_layer_destroy(lap_layer);
+
+    //Readd others
+    layer_add_child(window_get_root_layer(window),
+                    text_layer_get_layer(text_layer));
+    layer_add_child(window_get_root_layer(window),
+                    action_bar_layer_get_layer(action_bar));
+  }
+}
+
+void lap_menu_handle(ClickRecognizerRef recognizer, void *context)
+{
+  if (!inLapMenu)
+  {
+    inLapMenu = true;
+
+    //First, lets count the number of items
+    int numLaps = 0;
+    while (lapTimes[numLaps][2] == 'h')
+      numLaps++;
+
+    int i = 0;
+    for (i = 0; i < numLaps; i++)
+    {
+      menu_items[i] = (SimpleMenuItem){
+        .title = &lapTimes[i][0],
+      };
+    }
+
+    menu_sections[0] = (SimpleMenuSection){
+      .num_items = numLaps,
+      .items = menu_items,
+    };
+
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_frame(window_layer);
+    lap_layer = simple_menu_layer_create(bounds, window, menu_sections, 1, NULL);
+    layer_remove_child_layers(window_layer);
+    layer_add_child(window_layer, simple_menu_layer_get_layer(lap_layer));
+  }
+}
+
+void lap_menu_rel_handle(ClickRecognizerRef recognizer, void *context)
+{
+
+}
+
 void click_config_provider(void *context)
 {
   window_single_click_subscribe(BUTTON_ID_DOWN, (ClickHandler) lap_handle);
   window_single_click_subscribe(BUTTON_ID_UP, (ClickHandler) pause_handle);
   window_single_click_subscribe(BUTTON_ID_SELECT, (ClickHandler) reset_handle);
+  window_single_click_subscribe(BUTTON_ID_BACK, (ClickHandler) back_handle);
+  window_long_click_subscribe(BUTTON_ID_DOWN, 500, (ClickHandler) lap_menu_handle, 
+                                                   (ClickHandler) lap_menu_rel_handle);
 }
 
 static void window_load(Window *window) {
@@ -105,6 +214,7 @@ static void window_load(Window *window) {
   action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, my_icon_start);
   action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT, my_icon_reset);
   action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, my_icon_lap);
+
   time_t now = time(NULL);
   struct tm *current_time = localtime(&now);
   handle_second_tick(current_time, SECOND_UNIT);
